41cf22677f118bee6fa0ca11f3922d21c50d127c
youliangdao <youliangdaojing5@gmail.com>
2022-11-18 18:26:08 +0900

    🚀perf: getQueryDataを用いてキャッシュへの反映を確認してみる

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -1,15 +1,19 @@
+import { useQueryClient } from '@tanstack/react-query'
 import React, { FC, memo } from 'react'
 import { useQueryTags } from '../hooks/useQueryTags'
+import { Tag } from '../types/types'
 import TagItemMemo from './TagItem'
 
 const TagList: FC = () => {
   const { status, data } = useQueryTags()
+  // const queryClient = useQueryClient()
+  // const data = queryClient.getQueryData<Tag[]>(['tags'])
   console.log('rendered TagList')
   if (status === 'loading') return <div>{'Loading...'}</div>
   if (status === 'error') return <div>{'Error'}</div>
   return (
     <div>
-      {data.map((tag) => (
+      {data?.map((tag) => (
         <div key={tag.id}>
           <ul>
             <TagItemMemo tag={tag} />
